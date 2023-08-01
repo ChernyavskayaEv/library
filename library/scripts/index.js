fd04d7e6b279740c1efa89f9b82ec9726d765d97
Evgeniya Chernyavskaya <chev640@gmail.ru>
2023-08-01 22:23:55 +0700

    feat: implement adaptability for header and section welcome

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -1,4 +1,6 @@
 const score = `
+Library#1 - Фиксированная вёрстка
+
 1. Вёрстка валидная +10 - "Document checking completed. No errors or warnings to show."
 
 2. Вёрстка семантическая +16: 
@@ -58,3 +60,33 @@ const score = `
 `;
 
 console.log(score);
+
+const nav = document.querySelector('.header__nav');
+const bodyContent = document.querySelector('.body');
+const headerNavBtn = document.querySelector('.header__nav-btn');
+
+const hiddenNav = () => {
+  headerNavBtn.classList.remove('header__nav-btn_active');
+  nav.classList.remove('header__nav_active');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  bodyContent.addEventListener('click', (event) => {
+    if (event.target.closest('.header__nav-btn')) {
+      event.preventDefault();
+      headerNavBtn.classList.toggle('header__nav-btn_active');
+      nav.classList.toggle('header__nav_active');
+    }
+    if (
+      event.target.closest('.header__link') ||
+      event.target.closest('.main')
+    ) {
+      hiddenNav();
+    }
+  });
+  document.addEventListener('keyup', function (event) {
+    if (event.code === 'Escape') {
+      hiddenNav();
+    }
+  });
+});
